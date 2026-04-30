ea74c323e43b81a83ba27759439c343bb79bc063
paucolnav2 <pauadrielcolladonavarro2@gmail.com>
2026-04-30 16:33:21 +0200

    progress in implementation of http responses

--- src/main/java/com/itech/clientHandlers/HTTPHandler.java
+++ src/main/java/com/itech/clientHandlers/HTTPHandler.java
@@ -1,9 +1,14 @@
 package com.itech.clientHandlers;
 
+import com.itech.utils.helpers.HttpParser;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HTTPHandler {
     private BufferedReader input;
@@ -16,6 +21,28 @@ public class HTTPHandler {
     }
 
     public void handle (String firstLine) {
+        List<String> headerList = new ArrayList<>();
+//        List<String> bodyList = new ArrayList<>();
 
+        headerList.add(firstLine);
+        String line;
+        try {
+            while (!(line = input.readLine()).isEmpty()) {
+                headerList.add(line);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't read http message: "+e.getMessage());
+        }
+        String [] header = headerList.toArray(new String[0]);
+
+        String method = HttpParser.getMethod(header);
+        String route = HttpParser.getRoute(header);
+
+        if (method.equals("GET")) {
+            if (route.equals("/sensors")) {
+                SensorDAO.getAllSensors();
+            }
+        }
     }
 }
